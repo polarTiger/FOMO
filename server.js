d9ef5301b5a0a5d889cafec3473ca41e57dcf6c9
Seven Li <sevenlist0110@gmail.com>
2015-04-30 08:21:14 +0800

    [bugfix] fix merge conflict

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express=require('express');
-var app=express();
 var Bookshelf = require('bookshelf');
 var Knex=require('knex');
 
-<<<<<<< HEAD
-var express = require('express');
 var app = module.exports = express();
 var path = require('path');
 var bodyParser = require('body-parser');
@@ -33,8 +30,6 @@ var server = app.listen(3001, function () {
 
 
 
-=======
->>>>>>> 7130750269a0360a5f2e18e9feca310f1a5367f0
 var DB = Knex.initialize({
     client: 'mysql',
     connection: {
@@ -61,7 +56,6 @@ var eventTable = function (eventTable) {
   eventTable.increments('id').primary();
   eventTable.integer('eventID');
   eventTable.text('eventInfo');
-<<<<<<< HEAD
   eventTable.timestamps();
 };
 
@@ -87,7 +81,7 @@ var tagTable=function(tagTable) {
   tagTable.string('tagName');
   tagTable.timestamps();
 };
-=======
+
 }
 
 var userEventJointTable=function(userEventJointTable) {
@@ -97,7 +91,7 @@ var userEventJointTable=function(userEventJointTable) {
 }
 
 var 
->>>>>>> 7130750269a0360a5f2e18e9feca310f1a5367f0
+
 
 // this executes the schema operation:
 DB.schema.hasTable('users').then(function(exists) {
@@ -124,7 +118,7 @@ DB.schema.hasTable('usereventjoint').then(function(exists) {
   }
 });
 
-<<<<<<< HEAD
+
 DB.schema.hasTable('comments').then(function(exists) {
   if (! exists) {
     DB.schema.createTable('comments', commentTable).then(function () {
@@ -154,7 +148,4 @@ DB('movies')
    .then(function() { console.log("added"); })
    .catch(function(err) { console.log(err) });
 */
-<<<<<<< HEAD
 
-=======
->>>>>>> 7130750269a0360a5f2e18e9feca310f1a5367f0
