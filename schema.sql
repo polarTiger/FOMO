de1fd064af5197e6a8c5489145b98872e9c8b6ba
Seven Li <sevenlist0110@gmail.com>
2015-05-02 07:37:11 +0800

    [refactor] add more data to the schema table

--- schema.sql
+++ schema.sql
@@ -61,6 +61,14 @@ CREATE TABLE "users_events" (
 ALTER TABLE "users_events" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
 ALTER TABLE "users_events" ADD FOREIGN KEY ("event_id") REFERENCES "events" ("id");
 
-INSERT INTO users (name, hashed_password, email) values ('test', '1234', 'some@one.com');
-INSERT INTO events (event_info, event_title, event_category) values ('test event info', 'event1', 'concernt');
+INSERT INTO users (name, hashed_password, email) values ('John', '1234', 'some@one.com');
+INSERT INTO events (event_info, event_title, event_category) values ('test event info', 'event1', 'concert');
 INSERT INTO users_events (user_id, event_id) values (1, 1);
+
+INSERT INTO users (name, hashed_password, email) values ('Jane', '4567', 'jane@doe.com');
+INSERT INTO events (event_info, event_title, event_category) values ('test event info 2', 'event2', 'music');
+INSERT INTO users_events (user_id, event_id) values (2, 2);
+
+INSERT INTO users (name, hashed_password, email) values ('Bob', '9999', 'bob@bob.com');
+INSERT INTO events (event_info, event_title, event_category) values ('test event info 3', 'event3', 'album');
+INSERT INTO users_events (user_id, event_id) values (3, 3);
