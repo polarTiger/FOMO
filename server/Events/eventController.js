64b513b961ee438b6fde434e3e311b74da20879c
David Tan <davidtansf@gmail.com>
2015-05-04 15:46:42 -0700

    [feature] modify query to write to both notifications and events upon submission of new event;

--- server/Events/eventController.js
+++ server/Events/eventController.js
@@ -38,27 +38,83 @@ module.exports = {
   },
 
   addEvent: function(req, res) {
+    if (!req.body.notifydate) { // event only info, no notification, just insert into events table
 
-    var queryString = "INSERT into events (event_info, event_title, event_category, event_image, event_date) values ('"
-                              +req.body.info+"', '"+req.body.name+"', '"+req.body.category+"','"+req.body.link+"','"+req.body.date+"');";
+      var queryString = "INSERT into events (event_info, event_title, event_category, event_image, event_date) values ('"
+                                +req.body.info+"', '"+req.body.name+"', '"+req.body.category+"','"+req.body.link+"','"+req.body.date+"');";
 
-    pg.connect(dbUrl, function(err, client, done) {
-      if(err) {
-        return console.error('error fetching client from pool', err);
-      }
-      client.query(queryString, function(err, result) {
-      //call `done()` to release the client back to the pool
-        done();
+      pg.connect(dbUrl, function(err, client, done) {
         if(err) {
-          return console.error('error running query', err);
+          return console.error('error fetching client from pool', err);
         }
+        client.query(queryString, function(err, result) {
+        //call `done()` to release the client back to the pool
+          done();
+          if(err) {
+            return console.error('error running query', err);
+          }
 
-        res.end();
+          res.end();
 
-        client.end();
+          client.end();
+        });
       });
-    });
+    }
+    else {
+       // insert into multiple tables: http://stackoverflow.com/questions/20561254/insert-data-in-3-tables-at-a-time-using-postgres
+       console.log('insert into events and notifcations');
+      var queryString = "WITH first_insert AS (INSERT into events (event_info, event_title, event_category, event_image, event_date) values ('"
+                         +req.body.info+"', '"+req.body.name+"', '"+req.body.category+"','"+req.body.link+"','"+req.body.date+"') RETURNING id) INSERT into notifications (event_id, notification_info, notification_date, notification_time) SELECT id, '"
+                      +req.body.notifyinfo+"', '"+req.body.notifydate+"', '"+req.body.notifytime+"' FROM first_insert;";
+
+      pg.connect(dbUrl, function(err, client, done) {
+        if(err) {
+          return console.error('error fetching client from pool', err);
+        }
+        client.query(queryString, function(err, result) {
+        //call `done()` to release the client back to the pool
+          done();
+          if(err) {
+            return console.error('error running query', err);
+          }
+
+          res.end();
+
+          client.end();
+        });
+      });
+    }
   },
+
+// WITH ins1 AS (
+//    INSERT INTO sample(firstname, lastname)
+//    VALUES ('fai55', 'shaggk')
+//    RETURNING id
+//    )
+// , ins2 AS (
+//    INSERT INTO sample1 (user_id, adddetails)  -- assuming you want to target user_id
+//    SELECT id, 'ss'
+//    FROM   ins1
+//    RETURNING user_id
+//    )
+// INSERT INTO sample2 (user_id, value)          -- same here
+// SELECT id, 'ss - or something else'
+// FROM   ins1;
+
+// with first_insert as (
+//    insert into sample(firstname,lastname)
+//    values('fai55','shaggk')
+//    RETURNING id
+// ),
+// second_insert as (
+//   insert into sample1( id ,adddetails)
+//   values
+//   ( (select id from first_insert), 'ss')
+//   RETURNING user_id
+// )
+// insert into sample2 ( id ,adddetails)
+// values
+// ( (select user_id from first_insert), 'ss');
 
   searchEvents: function(req, res) {
     var clientQuery = req.query.query;
@@ -76,7 +132,6 @@ module.exports = {
       res.end(JSON.stringify(rows));
     });
   }
-
 
 };
 
