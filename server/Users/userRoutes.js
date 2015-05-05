64d38dc86b2cdaa28bb48b10cd87ddb13cd17ec4
Seven Li <sevenlist0110@gmail.com>
2015-05-06 02:47:32 +0800

    [refactor] fix merge conflict

--- server/Users/userRoutes.js
+++ server/Users/userRoutes.js
@@ -4,13 +4,9 @@ var userController = require('./userController.js');
 module.exports = function (app) {
   // app === userRouter injected from middlware.js
 
-<<<<<<< HEAD
   app.post('/signin', userController.signInPost, function(req, res) {
                             res.redirect('/');
                           });
-=======
-  app.post('/signin', userController.signInPost);
->>>>>>> [bug] trouble login
 
 
   app.post('/signup', userController.signUpPost);
