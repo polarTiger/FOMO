a0858467a4380340f8d28df36674ff03e460b4a5
Seven Li <sevenlist0110@gmail.com>
2015-05-06 03:14:55 +0800

    [refactor] fix merge conflict

--- client/controllers/signinController.js
+++ client/controllers/signinController.js
@@ -1,10 +1,7 @@
 angular.module('fomo.signin', [])
 
-<<<<<<< HEAD
 .controller('SigninController', ['$scope', '$http', '$state', '$log', 'UserService', function($scope, $http, $state, $log, UserService) {
-=======
-.controller('SigninController', ['$scope', '$http', '$log', 'UserService', function($scope, $http, $log, UserService) {
->>>>>>> [bug] trouble login
+
 
   $scope.signin = function() {
     var userObj = {
@@ -14,20 +11,12 @@ angular.module('fomo.signin', [])
 
     $http.post('api/users/signin', userObj)
       .success(function(data, status, headers, config) {
-<<<<<<< HEAD
         $log.log('success');
         $state.go('home');
     })
     .error(function(data, status, headers, config) {
       $log.log('fail');
       $state.go('signup');
-
-=======
-      $log.log('success');
-    })
-    .error(function(data, status, headers, config) {
-      $log.log('fail');
->>>>>>> [bug] trouble login
     });
   };
 }]);
--- client/controllers/signupController.js
+++ client/controllers/signupController.js
@@ -18,24 +18,10 @@ angular.module('fomo.signup', [])
 
     $http.post('api/users/signup', userObj).
       success(function(data, status, headers, config) {
-<<<<<<< HEAD
-<<<<<<< HEAD
       //$log.log('success');
     }).
     error(function(data, status, headers, config) {
       //$log.log('fail');
-=======
-      $log.log('success');
-    }).
-    error(function(data, status, headers, config) {
-      $log.log('fail');
->>>>>>> [feature] implement the signup front end and backend
-=======
-      //$log.log('success');
-    }).
-    error(function(data, status, headers, config) {
-      //$log.log('fail');
->>>>>>> [bug] trouble login
     });
   };
 }]);
