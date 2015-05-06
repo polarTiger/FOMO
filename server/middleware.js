bbd0eab259d6df5c952dd062cf30580ea9be1aa9
David Tan <davidtansf@gmail.com>
2015-05-06 16:13:42 -0700

    [feature] add favicon

--- server/middleware.js
+++ server/middleware.js
@@ -1,7 +1,8 @@
-var morgan = require('morgan'); 
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var express = require('express');
-
+var path = require('path');
+var favicon = require('serve-favicon');
 var session = require('express-session');
 var passport = require('./passport/passportConfig');
 
@@ -12,7 +13,7 @@ module.exports = function (app, express) {
   var eventRouter = express.Router();
 
   app.use(express.static(__dirname + '/../client'));
-
+  app.use(favicon(path.join(__dirname, '/../client/styles/favicon.ico')));
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
@@ -20,10 +21,10 @@ module.exports = function (app, express) {
   app.use(session({secret: 'abcde',resave: false, saveUninitialized: false, cookie: { httpOnly: false }}));
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use('/api/users', userRouter); 
+  app.use('/api/users', userRouter);
   app.use('/api/events', eventRouter);
 
-  
+
 
   require('./Users/userRoutes.js')(userRouter);
   require('./Events/eventRoutes.js')(eventRouter);
