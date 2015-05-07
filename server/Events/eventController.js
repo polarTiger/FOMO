9543eacd96eed96fe2ac8d5ebfad9a4afd4f0317
Seven Li <sevenlist0110@gmail.com>
2015-05-07 09:57:51 +0800

    [refactor] clean up the setInterval function

--- server/Events/eventController.js
+++ server/Events/eventController.js
@@ -50,30 +50,6 @@ setInterval(function(){
   //console.log((date.getMonth() + 1) + '-' + date.getDate() + '-' +  date.getFullYear());
 
   getEventFromDB(queryString, function(data){
-    console.log(data);
-    if (data[0].notification_date !== null) {
-      for (var i=0; i< data.length; i++) {
-        
-          var dbDate= new Date(data[0].notification_date);
-          var localTime= JSON.stringify(date).slice(12,17);
-          var localDate=JSON.stringify(date).slice(1,11);
-          dbDate=JSON.stringify(dbDate).slice(1,11);
-          console.log('local time is ',localTime);
-          console.log('local date is ', localDate);
-          var dbTime=data[0].notification_time.slice(0,5);
-          console.log('dbTime is :', dbTime);
-          console.log('dbDate is: ', dbDate);
-          console.log('local date type ', typeof localDate);
-          console.log('db date type ', typeof dbDate);
-          console.log(localDate === dbDate);
-          console.log(localTime=== dbTime);
-          if ( localDate === dbDate && localTime === dbTime) {
-              console.log('triggering:!!!');
-              module.exports.triggerEvent(i);
-          }
-        }
-    }
-    /*
     if (data[0].notification_date !== null) {
 
      var dbDate = new Date(data[0].notification_date);
@@ -94,7 +70,6 @@ setInterval(function(){
     }
 
   }
-  */
     
   });
 }, 1000*10); // update every 5 seconds
