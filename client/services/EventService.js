f5d5ee40793ca4ff968a803e795a8adf9f947d09
David Tan <davidtansf@gmail.com>
2015-05-09 16:58:04 -0700

    [feature] modify eventDate in frontend

--- client/services/EventService.js
+++ client/services/EventService.js
@@ -1,6 +1,6 @@
 angular.module('fomo.eventservice', [])
 
-.factory('EventService', ['$http', function($http) {
+.factory('EventService', ['$http', '$stateParams', '$log', function($http, $stateParams, $log) {
   var getEvent = function(eventId) {
     console.log('SERVICE: GET_EVENTS');
     //return $http.get('api/events/event/2')
@@ -22,8 +22,35 @@ angular.module('fomo.eventservice', [])
         return err;
       });
   };
+
+  var updateEventDate = function(eventObject) {
+
+    if (eventObject.eventdate) { // handles eventdate and eventtime, or just event date but no event time (defaults to 12:01 local)
+      var UTCdate = eventObject.eventdate.toJSON().slice(0,10);
+      var UTCeventtime = eventObject.eventtime ? eventObject.eventtime.toString().match(/\d{2}:\d{2}:\d{2}/)[0] : '00:01:00';
+      var UTCeventdate = eventObject.eventdate.toString().replace(/\d{2}:\d{2}:\d{2}/,UTCeventtime);
+      UTCeventdate = new Date(UTCeventdate).toJSON();
+      console.log('UTCeventdate: ', UTCeventdate);
+    }
+
+    var sendObject = {
+      eventdate: eventObject.eventdate ? UTCeventdate.slice(0,10) : null, // date and time converted to UTC time, ie. 7 or 8 hours ahead from Pacific
+      eventtime: UTCeventdate ? UTCeventdate.slice(11,16) : null // null when no event date, 12:01 local when no event time, or else user defined event time
+    };
+
+    console.log('SERVICE: UPDATE EVENT_DATE: ', sendObject);
+
+    return $http.post('/api/events/editevent/'+$stateParams.eventID, sendObject)
+      .success(function(data, status, headers, config) {
+        console.log('success update event date');
+      })
+      .error(function(data, status, headers, config) {
+        $log.log('fail');
+      });
+  };
   return {
     getEvent: getEvent,
-    submitEmail: submitEmail
+    submitEmail: submitEmail,
+    updateEventDate: updateEventDate
   };
 }]);
