b35ae24837f6256bee74e5fd9c4efe18072d5184
Trevor Caverly <trevorcaverly@gmail.com>
2015-05-11 16:18:33 -0700

    [feat] Added more info for event views and utility function for SELECT queries

--- client/services/UserService.js
+++ client/services/UserService.js
@@ -4,12 +4,6 @@ angular.module('fomo.userservice', [])
   var getallEvents = function() {
     console.log('SERVICE: GET_USER_EVENTS');
     return $http.get('/api/events/myevents');
-      // .success(function(resp) {
-      //   return resp;
-      // })
-      // .error(function(err) {
-      //   return err;
-      // });
   };
   return {
     getallEvents: getallEvents
