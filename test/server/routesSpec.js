c448aea2baf32420713b000a9937a87f6a225b80
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-05-19 19:20:29 -0700

    [test] add subscribe tests

--- test/server/routesSpec.js
+++ test/server/routesSpec.js
@@ -87,9 +87,9 @@ var user = { username : 'BigPete',
               password: 'test', 
               email : 'fake@gmail.com' };
 
-describe("User tests", function (){
+describe("User tests", function () {
 
-  it("expects to post a new user to /users", function(done){
+  it("expects to post a new user to /users", function(done) {
     agent
       .post("/api/users/signup")
       .send(user)
@@ -97,7 +97,7 @@ describe("User tests", function (){
   });
 
 
-  it("expects an unsuccessful login", function(done){
+  it("expects an unsuccessful login", function(done) {
     agent
       .post("/api/users/signin")
       .send({username: 'BigPete',
@@ -105,7 +105,7 @@ describe("User tests", function (){
       .expect(302, 'Moved Temporarily. Redirecting to /login', done);
   });
 
-  it("logs in user", function(done){
+  it("logs in user", function(done) { 
     agent
       .post("/api/users/signin")
       .send({username: 'BigPete',
@@ -114,7 +114,52 @@ describe("User tests", function (){
   });
 });
 
-describe("User authentication tests", function(){
+
+describe("user subscriptions", function() {
+  before(function(done){
+  agent.post("/api/users/signin")
+    .send({username: 'BigPete',
+          password: 'test'})
+    .end(function(err, res){
+      agent.saveCookies(res);
+      done();
+    });
+  });
+
+  describe("subscribe and unsubscribe", function() {
+
+    it("user should be allowed to subscribe", function(done) {
+      agent
+        .post("/api/events/subscribe/1")
+        .expect(200, done);
+    });
+
+    it("expects user to have subscribed to event", function(done) {
+      agent
+        .get("/api/events/myevents")
+        .expect(function(res) {
+          if (res.text[0].id === 1) {
+          } else {
+            return false;
+          }
+        }).end(done);
+      });
+    });
+
+    it("user should be allowed to subscribe", function(done) {
+      agent
+        .delete("/api/events/unsubscribe/1")
+        .expect(200, done);
+    });
+
+    it("expects user to have unsubscribed to event", function(done) {
+      agent
+        .get("/api/events/myevents")
+        .expect('[]', done);
+      });
+    });
+
+describe("User authentication tests", function() {
   before(function(done){
     agent.post("/api/users/signin")
       .send({username: 'BigPete',
@@ -125,52 +170,46 @@ describe("User authentication tests", function(){
       });
   });
 
-  describe("user logged in and logged out permissions", function(){
+  describe("user logged in and logged out permissions", function() {
 
-    it("expects user is logged in", function(done){
+    it("expects user is logged in", function(done) {
       agent
         .get("/api/users/signedin")
         .expect("BigPete", done);
     });
 
-    it("expects user is logged in", function(done){
+    it("expects user is logged in", function(done) {
       agent
         .get("/api/users/signedin")
         .expect("BigPete", done);
     });
 
-    it("expects to be allowed at myevents", function(done){
+    it("expects to be allowed at myevents", function(done) {
       agent
         .get("/api/events/myevents")
         .expect(200, done);
     });
-
-    it("expects to be allowed at subscribe", function(done){
-      agent
-        .post("/api/events/subscribe/1")
-        .expect(200, done);
-    });
     
-    it("expects to log out", function(done){
+    it("expects to log out", function(done) {
       agent
         .get("/api/users/signout")
         .expect(200, done);
     });
 
-    it("expects user to be logged out", function(done){
+    it("expects user to be logged out", function(done) {
       agent
         .get("/api/users/signedin")
         .expect({}, done);
     });
 
     describe("disallowed routes", function() {
-      it("expects to be denied at myevents", function(done){
+      it("expects to be denied at myevents", function(done) {
         agent
           .get("/api/events/myevents")
           .expect(403, done);
       });
 
-      it("expects to be denied at subscribe", function(done){
+      it("expects to be denied at subscribe", function(done) {
         agent
           .post("/api/events/subscribe/1")
           .expect(403, done);
